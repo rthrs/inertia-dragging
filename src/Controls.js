4950f0f030a20393c8b2c17f0f53525393bd133d
Artur Myszkowski <myszkowski.artur@gmail.com>
2020-01-03 18:34:48 +0100

    feature: better control style

--- src/Controls.js
+++ src/Controls.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import Switch from '@material-ui/core/Switch';
 import Tooltip from '@material-ui/core/Tooltip';
+import Snackbar from '@material-ui/core/Snackbar';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const Controls = ({ withInertia, toggleWithInertia }) => (
-  <div className="controls">
-    <span className="controls-text">
-      Drag seamless texture infinitely using pleasant inertia
-    </span>
-
+const Controls = ({ withInertia, toggleWithInertia }) => {
+  const inertiaControl = (
     <Tooltip title="Toggle inertia">
       <Switch 
         checked={withInertia}
         onChange={toggleWithInertia}
         value="withInertia"
-        color="primary"
       />
     </Tooltip>
-  </div> 
-);
+  );
+
+  const message = (
+    <React.Fragment>
+      <FormControlLabel
+        control={inertiaControl}
+        label="Drag seamless texture infinitely using pleasant inertia"
+        labelPlacement="start"
+      />
+      <div>
+        Made with React and RxJS
+      </div>
+    </React.Fragment>
+  );
+
+  return (
+    <Snackbar
+      open
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left'
+      }}
+      message={message}
+    />
+  );
+}
 
 export default Controls;
