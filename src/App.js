58ed9a8a0569bf159be4783561ad489696f2c8fc
Artur Myszkowski <myszkowski.artur@gmail.com>
2020-01-03 12:47:56 +0100

    feature: toggling inertia

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { compose } from 'ramda';
+import { compose, not } from 'ramda';
 
 import { dragging$, inertiaDragging$ } from './dragging.js';
 import { DEFAULT_TRANSLATE, TRANSLATE_MODULO, SCALE_FACTOR } from './constants.js';
@@ -9,20 +9,38 @@ const addTranslate = ({ dX, dY }) => ({ x, y }) => ({
   y: (y + dY) % TRANSLATE_MODULO
 })
 
+const ControlPopup = ({ withInertia, toggleWithInertia }) => (
+  <div className="control-popup">
+    <code>Drag seamless texture infinitely using pleasant inertia.</code>
+    <input type="checkbox" onChange={toggleWithInertia} checked={withInertia} />
+  </div> 
+)
+
 const App = () => {
   const [translate, setTransalte] = useState(DEFAULT_TRANSLATE);
+  const [withInertia, setWithInertia ] = useState(true);
 
   useEffect(() => {    
-    // dragging$.subscribe(compose(setTransalte, addTranslate));
-    inertiaDragging$.subscribe(compose(setTransalte, addTranslate));
-  }, []);
+    const stream$ = withInertia ? inertiaDragging$ : dragging$;
+    const subscription = stream$.subscribe(compose(setTransalte, addTranslate));
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [withInertia]);
 
   const { x, y } = translate;
   const transform = `translate(${x * SCALE_FACTOR}%, ${y * SCALE_FACTOR}%)`;
 
+  const toggleWithInertia = () => { 
+    setWithInertia(not); 
+  };
+
   return (
     <div className="dragging-container">
       <div className="dragging-background" style={{ transform }} />
+
+      <ControlPopup withInertia={withInertia} toggleWithInertia={toggleWithInertia} />
     </div>
   );
 }
--- src/dragging.js
+++ src/dragging.js
@@ -1,5 +1,5 @@
 import { fromEvent, interval, merge, animationFrameScheduler } from 'rxjs';
-import { takeUntil, mergeMap, pairwise, map, scan, withLatestFrom, takeWhile, tap, startWith } from 'rxjs/operators'
+import { takeUntil, mergeMap, pairwise, map, scan, withLatestFrom, takeWhile, tap, startWith, filter } from 'rxjs/operators';
 import { pick, map as mapR, multiply, nth, values, all, compose, lt } from 'ramda';
 import { AMORTIZATION, MOVE_THRESHOLD, DEFAULT_DIFF } from './constants';
 
@@ -30,20 +30,23 @@ export const dragging$ = mouseDrag$.pipe(
 
 const draggingWithDefaultOnMouseDown$ = mouseDown$.pipe(
   mergeMap(() => dragging$.pipe(startWith(DEFAULT_DIFF)))
-)
+);
+
+const allPassMoveThreshold = compose(all(lt(MOVE_THRESHOLD)), mapR(Math.abs), values);
 
 const inertia$ = mouseUp$.pipe(
   withLatestFrom(draggingWithDefaultOnMouseDown$),
   map(nth(1)),
+  filter(allPassMoveThreshold),
   tap((value) => console.log('Kick off inertia with: ', value)),
   mergeMap((value) => animationFrame$.pipe(
     scan(mapR(multiply(AMORTIZATION)), value),
-    takeWhile(compose(all(lt(MOVE_THRESHOLD)), mapR(Math.abs), values)),
+    takeWhile(allPassMoveThreshold),
     takeUntil(mouseDown$)
   ))
-)
+);
 
 export const inertiaDragging$ = merge(
   dragging$,
   inertia$
-)
+);
