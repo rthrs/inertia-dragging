04d5edda97eccf96d79651de489313d1decdd207
Artur Myszkowski <myszkowski.artur@gmail.com>
2020-01-03 13:59:28 +0100

    Updates

---
+++ precache-manifest.ab180e31236bcb7129809a3018d5baac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900f1cd95e1da75cf29d73881edd4d43",
+    "url": "/inertia-dragging/index.html"
+  },
+  {
+    "revision": "c4df05ee411a70620a39",
+    "url": "/inertia-dragging/static/css/main.7336f1be.chunk.css"
+  },
+  {
+    "revision": "a40dec4fd66482710167",
+    "url": "/inertia-dragging/static/js/2.e84da2bb.chunk.js"
+  },
+  {
+    "revision": "a18d72949a74316812e7b8fc42b1c1cc",
+    "url": "/inertia-dragging/static/js/2.e84da2bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4df05ee411a70620a39",
+    "url": "/inertia-dragging/static/js/main.52a6b855.chunk.js"
+  },
+  {
+    "revision": "74d545ead59ce34e95e1",
+    "url": "/inertia-dragging/static/js/runtime-main.2be4352e.js"
+  }
+]);
